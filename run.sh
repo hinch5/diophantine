73d9caf9685c8e576591b4e8ebbb90dc21d5b125
Oleg Shatnyuk <o.shatnuk@gmail.com>
2020-11-20 18:56:50 +0300

    script for test running

---
+++ run.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+if [[ ! -d 01_track ]]; then
+	curl -LO https://www.informatik.uni-kiel.de/~mku/woorpje/files/track01.tar.gz
+	tar -xvf track01.tar.gz
+	rm track01.tar.gz
+fi
+
+if [[ ! -d 05_track ]]; then
+        curl -LO https://www.informatik.uni-kiel.de/~mku/woorpje/files/track05.tar.gz
+        tar -xvf track05.tar.gz
+        rm track05.tar.gz
+fi
+
+if [[ $1 != "1" && $1 != "5" ]]; then
+	echo "enter track"
+	exit 1
+fi
+
+if [[ $2 == "" ]]; then
+	echo "enter task number"
+	exit 1
+fi
+
+grep "Equation" "0$1_track/woorpje/0$1_track_$2.eq" > tmp
+
+res="\$ENTRY Go\\n{ (e.Rules) =\\n<Eq (e.Rules) <Sim ( )\\n"
+
+while read -r line
+do
+	prefixed=${line#*Equation: }
+	lhs=${prefixed%=*}
+	rhs=${line#*= }
+	echo $lhs >tmp2
+	sed -e 's/\(.\)/\1 /g' <tmp2 >tmp3
+	lhs2=$(sed -e 's/\([A-Z]\)/\1s/g' <tmp3)
+	echo $rhs >tmp2
+	sed -e 's/\(.\)/\1 /g' <tmp2 >tmp3
+	rhs2=$(sed -e 's/\([A-Z]\)/\1s/g' <tmp3)
+	rm tmp2 tmp3
+	res="$res <Decode (<Conc ('$lhs2 =')\\n' ${rhs2::-1}' >)> \\n"
+done < tmp
+rm tmp
+
+res="$res >>;\\n}\\n"
+
+rm -r solutions/0$1_track_$2
+mkdir -p solutions/0$1_track_$2
+
+sed -e "s/\\\$ENTRY Go { (e.Rules) = <Eq (e.Rules) <Sim ( ) <Decode (<Conc ('As =') ' Cs'>)>>>;}/$res/g" <weqs_int_bench_task.ref >solutions/0$1_track_$2/weqs_int_bench_0$1_track_$2.ref
+
+inref4p solutions/0$1_track_$2/weqs_int_bench_0$1_track_$2.ref
+scp4 weqs_int_bench_0$1_track_$2.mst
+cgr54 solutions/0$1_track_$2/result.ref
